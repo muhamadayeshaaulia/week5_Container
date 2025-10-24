778a427bbc8194de03619866f71b05e251f0e94d
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-10-24 20:57:41 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'latihan flutter week5',
+      home: Myhome(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Myhome extends StatelessWidget {
+  const Myhome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('latihan flutter week5'),
+        backgroundColor: Colors.blue,
+        shape: border,
+      ),
+    );
+  }
+}
